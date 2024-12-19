5549e31a7cf3f4726c7f492f5cb585e24b6e2691
Sewlesew Biazen <sewlesewbiazen65@gmail.com>
2024-12-19 15:31:21 +0300

    page layout setup

---
+++ app/(root)/home/page.tsx
@@ -0,0 +1,10 @@
+import PageContent from '@/app/_components/layout/pageContent'
+import React from 'react'
+
+const Page = async () => {
+    return (
+        <PageContent>Page</PageContent>
+    )
+}
+
+export default Page
